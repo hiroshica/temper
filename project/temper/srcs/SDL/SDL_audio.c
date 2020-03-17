2414f681b9dcc20ed661d7a9248600009df33e0c
hiroshica <hiroshica@gmail.com>
2020-03-17 16:51:14 +0900

    AudioをSDL2に置き換えた

--- project/temper/srcs/SDL/SDL_audio.c
+++ project/temper/srcs/SDL/SDL_audio.c
@@ -1,7 +1,8 @@
 #include "../common.h"
 
-SDL_mutex *audio_mutex;
-SDL_cond *audio_cv;
+static SDL_mutex *audio_mutex;
+static SDL_cond *audio_cv;
+static int AudioDeviceId;
 
 /*
  * thread A
@@ -18,16 +19,16 @@ void audio_callback(void *userdata, Uint8 *stream, int length)
 
   SDL_LockMutex(audio_mutex);
 
-  while(((audio.buffer_index - audio.buffer_base) %
-   AUDIO_BUFFER_SIZE) < length)
+  while (((audio.buffer_index - audio.buffer_base) %
+          AUDIO_BUFFER_SIZE) < length)
   {
     // Pump remaining cycles if you can.
     SDL_CondWait(audio_cv, audio_mutex);
   }
 
-  if(config.enable_sound)
+  if (config.enable_sound)
   {
-    if((audio.buffer_base + sample_length) >= AUDIO_BUFFER_SIZE)
+    if ((audio.buffer_base + sample_length) >= AUDIO_BUFFER_SIZE)
     {
       u32 partial_length = (AUDIO_BUFFER_SIZE - audio.buffer_base) * 2;
       sound_copy(audio.buffer_base, partial_length, normal);
@@ -42,7 +43,7 @@ void audio_callback(void *userdata, Uint8 *stream, int length)
   }
   else
   {
-    if((audio.buffer_base + sample_length) >= AUDIO_BUFFER_SIZE)
+    if ((audio.buffer_base + sample_length) >= AUDIO_BUFFER_SIZE)
     {
       u32 partial_length = (AUDIO_BUFFER_SIZE - audio.buffer_base) * 2;
       audio.buffer_base = (length - partial_length) / 2;
@@ -54,7 +55,7 @@ void audio_callback(void *userdata, Uint8 *stream, int length)
     memset(stream, 0, length);
   }
 
-  if(config.fast_forward == 0)
+  if (config.fast_forward == 0)
     SDL_CondSignal(audio_cv);
 
   SDL_UnlockMutex(audio_mutex);
@@ -66,27 +67,27 @@ void audio_callback(void *userdata, Uint8 *stream, int length)
 void initialize_audio()
 {
   printf("initializing audio with frequency %d\n",
-   config.audio_output_frequency);
+         config.audio_output_frequency);
 
-  switch(config.audio_output_frequency)
+  switch (config.audio_output_frequency)
   {
-    case 8000:
-    case 11025:
-      audio.playback_buffer_size = 512;
-      break;
+  case 8000:
+  case 11025:
+    audio.playback_buffer_size = 512;
+    break;
 
-    case 16000:
-    case 22050:
-      audio.playback_buffer_size = 1024;
-      break;
+  case 16000:
+  case 22050:
+    audio.playback_buffer_size = 1024;
+    break;
 
-    case 44100:
-      audio.playback_buffer_size = 2048;
-      break;
+  case 44100:
+    audio.playback_buffer_size = 2048;
+    break;
 
-    default:
-      audio.playback_buffer_size = 4096;
-      break;
+  default:
+    audio.playback_buffer_size = 4096;
+    break;
   }
 
   audio.pause_state = 1;
@@ -94,20 +95,43 @@ void initialize_audio()
   //audio.playback_buffer_size = 2048;
   //audio.playback_buffer_size = 4096;
   //audio.playback_buffer_size = 16384;
-
-  SDL_AudioSpec desired_spec =
-  {
-    config.audio_output_frequency, AUDIO_S16SYS, 2, 0,
-    audio.playback_buffer_size / 4, 0, 0, audio_callback, NULL
-  };
-
+  const char *getname;
+  int retcode;
+  int i;
+  int count = SDL_GetNumAudioDevices(0);
+  SDL_AudioSpec desired_spec; // = { config.audio_output_frequency, AUDIO_S16SYS, 2, 0, audio.playback_buffer_size / 4, 0, 0, audio_callback, NULL};
   SDL_AudioSpec audio_settings;
-  SDL_OpenAudio(&desired_spec, &audio_settings);
-  SDL_PauseAudio(1);
 
-  audio.output_frequency = audio_settings.freq;
-  audio_mutex = SDL_CreateMutex();
-  audio_cv = SDL_CreateCond();
+  for (i = 0; i < count; ++i)
+  {
+    SDL_Log("オーディオデバイス %d: %s", i, SDL_GetAudioDeviceName(i, 0));
+  }
+  if (count > 0)
+  {
+    SDL_memset(&desired_spec, 0, sizeof(desired_spec)); /* または SDL_zero(want); */
+    desired_spec.freq = config.audio_output_frequency;
+    desired_spec.format = AUDIO_S16SYS;
+    desired_spec.channels = 2;
+    desired_spec.samples = audio.playback_buffer_size / 4;
+    desired_spec.callback = audio_callback; /* この関数はどこか別の場所に書く. 詳細はSDL_AudioSpecを参照すること */
+    getname = SDL_GetAudioDeviceName(0, 0);
+    AudioDeviceId = SDL_OpenAudioDevice(
+        getname,
+        0,
+        &desired_spec, &audio_settings,
+        SDL_AUDIO_ALLOW_ANY_CHANGE);
+    if (AudioDeviceId == 0)
+    {
+      printf("!!!! not create SDL Audio abort.\n error message:%s\n", SDL_GetError());
+    }
+    else{
+      SDL_PauseAudioDevice(AudioDeviceId,1);
+
+      audio.output_frequency = audio_settings.freq;
+      audio_mutex = SDL_CreateMutex();
+      audio_cv = SDL_CreateCond();
+    }
+  }
 }
 
 void audio_exit()
@@ -117,9 +141,11 @@ void audio_exit()
   SDL_CondSignal(audio_cv);
   SDL_LockMutex(audio_mutex);
   SDL_UnlockMutex(audio_mutex);
+
+  SDL_CloseAudioDevice(AudioDeviceId);
+
   SDL_DestroyCond(audio_cv);
   SDL_DestroyMutex(audio_mutex);
-  SDL_CloseAudio();
 }
 
 // Do not do either of these two without first locking/unlocking audio
@@ -133,12 +159,12 @@ void audio_signal_callback()
 
 void audio_wait_callback()
 {
-  if((((audio.buffer_index - audio.buffer_base) %
-   AUDIO_BUFFER_SIZE) > (audio.playback_buffer_size * 3 / 2)) &&
-   (config.fast_forward == 0))
+  if ((((audio.buffer_index - audio.buffer_base) %
+        AUDIO_BUFFER_SIZE) > (audio.playback_buffer_size * 3 / 2)) &&
+      (config.fast_forward == 0))
   {
-    while(((audio.buffer_index - audio.buffer_base) %
-     AUDIO_BUFFER_SIZE) > (audio.playback_buffer_size * 3 / 2))
+    while (((audio.buffer_index - audio.buffer_base) %
+            AUDIO_BUFFER_SIZE) > (audio.playback_buffer_size * 3 / 2))
     {
       SDL_CondWait(audio_cv, audio_mutex);
     }
@@ -159,19 +185,34 @@ u32 audio_pause()
 {
   u32 current_audio_pause = audio.pause_state;
 
-  if(current_audio_pause == 0)
-    SDL_PauseAudio(1);
-
-  audio.pause_state = 1;
+  if (current_audio_pause == 0)
+  {
+    switch (SDL_GetAudioStatus())
+    {
+    case SDL_AUDIO_STOPPED:
+      printf("停止中\n");
+      break;
+    case SDL_AUDIO_PLAYING:
+      printf("再生中\n");
+      SDL_PauseAudioDevice(AudioDeviceId,1);
+      audio.pause_state = 1;
+      break;
+    case SDL_AUDIO_PAUSED:
+      printf("一時停止中\n");
+      break;
+    default:
+      printf("???");
+      break;
+    }
+  }
   return current_audio_pause;
 }
 
 void audio_unpause()
 {
-  if(audio.pause_state)
+  if (audio.pause_state)
   {
     audio.pause_state = 0;
-    SDL_PauseAudio(0);
+    SDL_PauseAudioDevice(AudioDeviceId,0);
   }
 }
-
